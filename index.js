07de641daa3a7c1d48f6e6b1393fa849cb34d82f
Daniel <oopdaniel@gmail.com>
2018-10-11 17:47:33 -0700

    add random number inputs

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import fs from 'fs'
 
 const FILE_NAME_IN = 'input.txt'
 const file = fs.readFileSync(`./${FILE_NAME_IN}`, 'utf8')
-const lines = file.split('\n')
+const arr = file.split(', ').map(c => +c)
