b147eb76994bb2a8dd7454f9c6f6593259508804
Daniel <oopdaniel@gmail.com>
2018-10-12 09:56:03 -0700

    support solve

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import fs from 'fs'
+import { clock, printTime } from './utils'
+import solve from './solve'
+
+const now = clock()
 
 const FILE_NAME_IN = 'input.txt'
 const file = fs.readFileSync(`./${FILE_NAME_IN}`, 'utf8')
 const arr = file.split(', ').map(c => +c)
+
+solve(arr)
+
+printTime(clock(now))
+
+console.log('Result: ', arr)
---
+++ utils.js
@@ -0,0 +1,7 @@
+export function clock (start) {
+  if (!start) return process.hrtime()
+  var end = process.hrtime(start)
+  return Math.round((end[0] * 1000) + (end[1] / 1000000))
+}
+
+export const printTime = (time) => console.log(`Took ${time} ms.`)
